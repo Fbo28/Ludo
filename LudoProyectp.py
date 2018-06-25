88643a0eff62295c1f46958aaf3daa2997360621
TaquitoTwister <40031728+TaquitoTwister@users.noreply.github.com>
2018-06-25 18:05:49 -0500

    Update LudoProyectp.py

--- LudoProyectp.py
+++ LudoProyectp.py
@@ -11,10 +11,27 @@ rojo = (255,0,0)
 azul = (0,0,255)
 verde = (0,255,0)
 amarillo = (255,255,0)
+fondo = (255,250,205)
 
 #tabero
-tm=pygame.display.set_mode((900,600))
-pygame.display.set_caption("LudoMania")
+tm=pygame.display.set_mode((900,700))
+pygame.display.set_caption("LudoParaPobres")
+tm.fill(fondo)
+
+#board
+pygame.draw.line(tm,negro,(30,30),(30,670),3)
+pygame.draw.line(tm,negro,(30,30),(870,30),3)
+pygame.draw.line(tm,negro,(30,670),(870,670),3)
+pygame.draw.line(tm,negro,(870,30),(870,670),3)
+pygame.draw.line(tm,negro,(770,30),(770,670),3)
+pygame.draw.circle(tm,rojo,(130,570),90,4)
+pygame.draw.circle(tm,verde,(670,570),90,4)
+pygame.draw.circle(tm,azul,(130,130),90,4)
+pygame.draw.circle(tm,amarillo,(670,130),90,4)
+pygame.draw.circle(tm,negro,(130,570),93,4)
+pygame.draw.circle(tm,negro,(670,570),93,4)
+pygame.draw.circle(tm,negro,(130,130),93,4)
+pygame.draw.circle(tm,negro,(670,130),93,4)
 
 while True:
     for evento in pygame.event.get():
@@ -23,4 +40,6 @@ while True:
             sys.exit()
 
         pygame.display.update()
+
+
 
